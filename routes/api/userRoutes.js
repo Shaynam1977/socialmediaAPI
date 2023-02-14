cc244706ddad53fd4934b698f3956fd3cbbcf2d9
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2023-02-14 10:54:20 -0800

    user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  unFriend,
+} = require('../../controllers/courseController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+  router.route("/:userId/friends/:friendId").post(createFriend).delete(unFriend)
+module.exports = router;
