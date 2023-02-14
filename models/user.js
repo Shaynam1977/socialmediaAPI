2a3ea62f554a1b6f911ca67e28e549cee45564b1
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2023-02-14 10:33:08 -0800

    schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required:true,
+      match:[/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, "you need a valid email"]
+    },
+    
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts',
+      },
+      
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    ]
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model('users', userSchema);
+
+module.exports = User;
